b4044c3613d22bb22fa07971a4b0daf917b2df55
Ioana Simtion <ionitza6@gmail.com>
2023-06-22 15:38:47 +0300

    done varianta31

---
+++ varianta31/main.cpp
@@ -0,0 +1,19 @@
+/**Se consideră o listă liniară simplu înlănţuită alocată dinamic în care fiecare nod memorează
+în câmpul info un cuvânt format din cel mult 20 de caractere, doar litere mari ale
+alfabetului englez, iar în câmpul urm, adresa următorului nod al listei sau NULL dacă nu
+există un element următor.
+Scrieţi un program C/C++ care citeşte de la tastatură un număr natural n (n≤100) şi apoi n
+cuvinte distincte, fiecare cuvânt fiind format din cel mult 20 de caractere, doar litere mari ale
+alfabetului englez, şi construieşte o listă simplu înlănţuită, cu acele cuvinte citite, care încep
+şi se termină cu aceeaşi literă. Cuvintele se vor memora în listă în ordine inversă citirii lor. */
+
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+
+    return 0;
+}
---
+++ varianteBac2009/varianta31.h
@@ -0,0 +1,119 @@
+﻿using namespace std;
+#include <iostream>
+#include <fstream>
+#pragma once
+
+
+
+//SUBIECTUL 3
+
+
+
+//3
+/*Scrieţi definiţia completă a subprogramului suma care primeşte ca parametru un tablou 
+unidimensional x cu cel mult 100 de elemente, numere reale, un număr natural n ce 
+reprezintă numărul efectiv de elemente ale tabloului x (n≤100), şi un număr natural m (n≥m). 
+Subprogramul returnează suma obţinută din cele mai mici m elemente ale tabloului x.  */
+
+
+void citireVector(int v[], int& n) {
+
+	ifstream f("vectorOrdonare.txt");
+
+	f >> n;
+
+	for (int i = 0; i < n; i++) {
+		f >> v[i];
+	}
+
+}
+
+
+int suma(int v[], int n, int m) {
+
+	int s = 0;
+	bool flag = false;
+
+	do {
+		flag = true;
+		for (int i = 0; i < n - 1; i++) {
+			if (v[i] > v[i + 1]) {
+				int aux = v[i];
+				v[i] = v[i + 1];
+				v[i + 1] = aux;
+				flag = false;
+			}
+		}
+	} while (flag == false);
+
+	for (int i = 0; i < m; i++) {
+		s += v[i];
+	}
+
+	return s;
+
+}
+
+
+void solutie3() {
+
+	int v[100], n, m;
+
+	cout << "m=";
+	cin >> m;
+
+	citireVector(v, n);
+
+	cout << suma(v, n, m);
+
+}
+
+
+
+//4
+/*În fişierul numere.txt se află memorate, pe prima linie un număr natural n (1≤n≤100), iar 
+pe fiecare dintre următoarele n linii, câte două numere întregi x,y (-100≤x≤y≤100),
+reprezentând capetele câte unui segment [x,y] desenat pe axa Ox de coordonate. 
+a) Scrieţi în limbajul C/C++ un program eficient din punct de vedere al timpului de executare
+şi al spaţiului de memorare, care citeşte din fişier datele existente, determină segmentul 
+rezultat în urma intersecţiei tuturor celor n segmente date şi afişează pe ecran două numere 
+despărţie printr-un spaţiu ce reprezintă capetele segmentului cerut. Dacă segmentele nu au 
+nici un punct comun se va afişa pe ecran valoarea 0. */
+
+
+void citireAfisareCoordonate() {
+
+	ifstream f("coordonateSegmente.txt");
+
+	int x, y, n, xMax, yMin;
+
+	f >> n;
+	f >> x >> y;
+	xMax = x;
+	yMin = y;
+
+	for (int i = 0; i < n; i++) {
+		f >> x >> y;
+		if (x > xMax) {
+			xMax = x;
+		}
+		if (y < yMin) {
+			yMin = y;
+		}
+	}
+
+	if (xMax < yMin) {
+		cout << xMax << ' ' << yMin;
+	}
+	else {
+		cout << 0;
+	}
+
+}
+
+
+void solutie4() {
+
+	citireAfisareCoordonate();
+
+}
